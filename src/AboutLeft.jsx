36159a8c23e54f4f9c407f68d8e85288c99cae96
regul4rn0name <92522298+regul4rn0name@users.noreply.github.com>
2025-05-10 20:03:34 +0200

    About component

---
+++ src/AboutLeft.jsx
@@ -0,0 +1,12 @@
+export default function AboutLeft(){
+    return(
+        <div className="h-full w-full col-span-2 row-span-2 self-start justify-center items-center m-auto flex flex-col border-r-2 border-stone-400">
+        <img className="rounded-full h-[250px] w-[250px]" src="/example.jpeg"/>
+        <br/>
+        <h1 className="font-ubuntu text-cyan-600 text-2xl">Małgorzata Buczek</h1>
+        <br/>
+        <h2 className="font-ubuntu text-cyan-600 text-xl">placeholder</h2>
+    </div>
+    )
+    
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./About"
+
 function App() {
 
 
   return (
     <>
-
+      <About/>
     </>
   )
 }
