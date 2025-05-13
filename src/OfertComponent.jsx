c9e84a1b7d132460cca6b833e549ae9a9d63460e
Bohdan <bohdanrebman@gmail.com>
2025-05-13 12:39:59 +0200

    Add Oferts element

--- src/OfertComponent.jsx
+++ src/OfertComponent.jsx
@@ -1,13 +1,14 @@
 export default function Ofer_element(){
     return(
-        <div className="flex flex-row h-96 w-96 mx-2  bg-blue-300 rounded-xl">
-            <section className="w-1/2 rounded-l-xl flex flex-col text-center">
-                <h3>Lekcja</h3>
+        <div className="flex flex-row h-96 w-96 mx-2  bg-white rounded-2xl  border-solid border-cyan-600 border-3">
+            <section className="w-1/2 rounded-l-xl flex flex-col text-center text-wrap px-3">
+                <h1 className="text-xl font-medium mt-3 ">Lekcja</h1>
                 <br />
-                <h2>Opis</h2>
-                <h2>34zl</h2>
+                <h2>Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym. Został po raz pierwszy użyty w XV w.</h2>
+                <br />
+                <h3 className="mt-10 text-[18px]">34zl</h3>
             </section>
-            <section className="w-1/2 bg-[url('tenis.jpg')] bg-cover bg-center rounded-xl">
+            <section className="w-2/4 bg-[url('tenis.jpg')] bg-cover bg-center rounded-xl">
             </section>
         </div>
     )
--- src/Oferts.jsx
+++ src/Oferts.jsx
@@ -1,9 +1,9 @@
 import OfertComponent from "./OfertComponent"
 export default function Oferts(){
     return(
-        <div>
-            <h1 className="text-blue-700 text-4xl justify-center flex mt-5">Oferty</h1>
-            <div className="overflow-x-scroll whitespace-nowrap flex flex-row mt-9">
+        <div className=" mb-5 ">
+            <h1 className="text-black text-5xl justify-center flex mt-5 font-medium">Oferty</h1>
+            <div className="overflow-x-scroll whitespace-nowrap flex flex-row mt-9 no-scrollbar">
                 <div className="flex h-96">
                     <OfertComponent/>
                     <OfertComponent/>
